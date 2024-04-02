17b6fba485b2d38d7f1d2373d568490c7d02c411
DNEGEL3125 <2839212140@qq.com>
2024-04-02 13:59:51 +0800

    ProductRepository is available
    Signed-off-by: DNEGEL3125 <2839212140@qq.com>

--- src/main/java/cn/csu/mypetstore_springboot/Repositories/ProductRepository.java
+++ src/main/java/cn/csu/mypetstore_springboot/Repositories/ProductRepository.java
@@ -2,6 +2,9 @@ package cn.csu.mypetstore_springboot.Repositories;
 
 import cn.csu.mypetstore_springboot.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,5 +13,10 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 
     Product getProductByProductId(String productId);
 
+    @Transactional
+    @Modifying
+    @Query(value = """
+            select product_id, NAME, description, category_id
+             from PRODUCT WHERE lower(name) like :keyword""", nativeQuery = true)
     List<Product> searchProductsByName(String keyword);
 }
--- src/main/java/cn/csu/mypetstore_springboot/controllers/MainController.java
+++ src/main/java/cn/csu/mypetstore_springboot/controllers/MainController.java
@@ -1,16 +1,17 @@
 package cn.csu.mypetstore_springboot.controllers;
 
-import cn.csu.mypetstore_springboot.Repositories.ItemRepository;
-import cn.csu.mypetstore_springboot.domain.Item;
+import cn.csu.mypetstore_springboot.Repositories.ProductRepository;
+import cn.csu.mypetstore_springboot.domain.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class MainController {
     Logger logger = LoggerFactory.getLogger(MainController.class);
-
 
     @GetMapping("/main")
     public String doGet() {
