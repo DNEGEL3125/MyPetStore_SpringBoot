ee3245f91fee4fc5db2df4e3b3a964275e3288dd
DNEGEL3125 <2839212140@qq.com>
2024-04-18 13:09:29 +0800

    Color the logs;
    Signed-off-by: DNEGEL3125 <2839212140@qq.com>

--- src/main/java/cn/csu/mypetstore_springboot/MyPetStoreSpringBootApplication.java
+++ src/main/java/cn/csu/mypetstore_springboot/MyPetStoreSpringBootApplication.java
@@ -3,6 +3,7 @@ package cn.csu.mypetstore_springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO 实现一个可以查看log的界面
 @SpringBootApplication
 public class MyPetStoreSpringBootApplication {
 
